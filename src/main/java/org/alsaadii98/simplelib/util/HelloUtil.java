e98665f249c0337651917abd67a28025c3a90f09
Ali Abdulhadi <a.h.alsaady1998@gmail.com>
2024-04-28 21:12:28 +0300

    fixes

--- src/main/java/org/alsaadii98/simplelib/util/HelloUtil.java
+++ src/main/java/org/alsaadii98/simplelib/util/HelloUtil.java
@@ -1,5 +1,8 @@
 package org.alsaadii98.simplelib.util;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloUtil {
     public static String sayHello() {
         return "Hello, World";
